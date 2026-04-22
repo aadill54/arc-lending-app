dfa25fa1c31c2f7cd7f889a5b27f372d21650b6a
aadill54 <dheerajladha36@gmail.com>
2026-04-22 15:02:35 +0530

    fix typescript error

--- app/providers.tsx
+++ app/providers.tsx
@@ -20,7 +20,7 @@ const config = createConfig({
 
 const queryClient = new QueryClient()
 
-export function Providers({ children }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
